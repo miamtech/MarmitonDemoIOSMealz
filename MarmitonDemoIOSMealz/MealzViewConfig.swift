35a664ade9f48280af1fc5cdac1ca5f5e8985de8
walerowicz-miam <damien.walerowicz@mealz.ai>
2024-05-02 15:55:44 +0200

    add checkout webview

--- MarmitonDemoIOSMealz/MealzViewConfig.swift
+++ MarmitonDemoIOSMealz/MealzViewConfig.swift
@@ -12,6 +12,8 @@ import MealzIOSFramework
 import MealzNavModuleIOS
 import mealzcore
 import UIKit
+import WebKit
+
 
 let changeStore: () -> Void = {
     let htmlFileURL =  Bundle(identifier: "MarmitonUIMealzIOS-MarmitonUIMealzIOS-resources")!.url(forResource: "index", withExtension: "html", subdirectory: "Ressources")!
@@ -23,6 +25,37 @@ let changeStore: () -> Void = {
     
     UIApplication.shared.keyWindow?.rootViewController?.present(mealsWebView, animated: true)
     
+}
+
+let showCheckout: (_ url:String?) -> Void = {url in 
+    guard let stringUrl = url , let url = URL(string:stringUrl) else { return }
+     let request = URLRequest(url: url)
+    
+    let config = WKWebViewConfiguration()
+    let webView = WKWebView(frame: .zero, configuration: config)
+    webView.load(request)
+    webView.translatesAutoresizingMaskIntoConstraints = false
+    
+    let viewController = UIViewController()
+    viewController.view.addSubview(webView)
+    NSLayoutConstraint.activate([
+        webView.leadingAnchor.constraint(equalTo: viewController.view.leadingAnchor, constant: 0),
+        webView.trailingAnchor.constraint(equalTo: viewController.view.trailingAnchor, constant: 0),
+        webView.topAnchor.constraint(equalTo: viewController.view.topAnchor, constant: 0),
+        webView.bottomAnchor.constraint(equalTo: viewController.view.bottomAnchor, constant: 0),
+    ])
+    
+    if let sceneDelegate = UIApplication.shared.connectedScenes
+        .first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene,
+       let keyWindow = sceneDelegate.windows.first(where: { $0.isKeyWindow }),
+       let rootViewController = keyWindow.rootViewController {
+
+        var topViewController = rootViewController
+        while let presentedViewController = topViewController.presentedViewController {
+            topViewController = presentedViewController
+        }
+        topViewController.present(viewController, animated: true)
+    }
 }
 
 struct MealzViewConfig {
@@ -114,7 +147,9 @@ struct MealzViewConfig {
     
     static let myBasketView = MyBasketViewOptions(
         title: TypeSafeBaseTitle(MarmitonMyBasketTitle(changeStore: changeStore)),
-        swapper: TypeSafeMyBasketSwapper(MarmitonMyBasketSwapper(onAddAnotherProduct: {}))
+        swapper: TypeSafeMyBasketSwapper(MarmitonMyBasketSwapper(onAddAnotherProduct: {})),
+        footer: TypeSafeMyBasketFooter(MarmitonMyBasketFooter(onSubmitOrder: { url in           
+        }))
     )
     
     static let myBasketConfig = MyBasketFeatureConstructor(
@@ -123,7 +158,8 @@ struct MealzViewConfig {
         myMealsBaseViews: myMealsBaseView,
         myProductsBaseViews: myProductsBaseView,
 //        catalogRecipesListGridConfig: myMealsGridConfig,
-        navigateToCatalog: showCatalog
+        navigateToCatalog: showCatalog,
+        navigateToCheckout: showCheckout
     )
     
 }
