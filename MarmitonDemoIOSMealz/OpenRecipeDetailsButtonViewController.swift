6434fd3c11ce2e8275bb99b797fcdb806608501a
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2024-06-14 11:20:04 +0200

    remove basket service

--- MarmitonDemoIOSMealz/OpenRecipeDetailsButtonViewController.swift
+++ MarmitonDemoIOSMealz/OpenRecipeDetailsButtonViewController.swift
@@ -11,165 +11,160 @@ import mealzcore
 
 class OpenRecipeDetailsButtonViewController: UIViewController {
    
-   let recipeDetailsLabel = UILabel()
-   let recipeDetailsButton = UIButton(type: .system) // .system gives you the 'UIKit' look
+   let recipeDetailsLabel1 = UILabel()
+   let recipeDetailsButton1 = UIButton(type: .system)
    
-   let changeStoreLabel = UILabel()
-   let changeStoreToggle = UISwitch()
+   let priceOfRecipeButton1 = UIButton(type: .system)
+   var priceOfRecipe1: String? = nil
    
-   let changeUserLabel = UILabel()
-   let changeUserToggle = UISwitch()
+   let recipeDetailsLabel2 = UILabel()
+   let recipeDetailsButton2 = UIButton(type: .system)
    
-   let priceOfRecipeButton = UIButton(type: .system)
-   var priceOfRecipe: String? = nil
+   let priceOfRecipeButton2 = UIButton(type: .system)
+   var priceOfRecipe2: String? = nil
    
    override func viewDidLoad() {
       super.viewDidLoad()
       
-      /* ----------------------------- RECIPE DETAILS -----------------------------------------------*/
+      /* ----------------------------- RECIPE DETAILS 1 -----------------------------------------------*/
       // Configure the label
-      recipeDetailsLabel.translatesAutoresizingMaskIntoConstraints = false
-      recipeDetailsLabel.text = "Sample Recipe Details"
-      recipeDetailsLabel.textAlignment = .center
+      recipeDetailsLabel1.translatesAutoresizingMaskIntoConstraints = false
+      recipeDetailsLabel1.text = "Sample Recipe 1"
+      recipeDetailsLabel1.textAlignment = .center
       
       // Configure the button
-      recipeDetailsButton.translatesAutoresizingMaskIntoConstraints = false
-      recipeDetailsButton.setTitle("Launch", for: .normal)
-      recipeDetailsButton.addTarget(self, action: #selector(launchRecipeDetails), for: .touchUpInside)
+      recipeDetailsButton1.translatesAutoresizingMaskIntoConstraints = false
+      recipeDetailsButton1.setTitle("Launch", for: .normal)
+      recipeDetailsButton1.addTarget(self, action: #selector(launchRecipeDetails1), for: .touchUpInside)
       
-      /* ----------------------------- CHANGE STORE -----------------------------------------------*/
+      // Configure the button
+      priceOfRecipeButton1.translatesAutoresizingMaskIntoConstraints = false
+      priceOfRecipeButton1.setTitle(priceOfRecipe1 ?? "Get Price", for: .normal)
+      priceOfRecipeButton1.addTarget(self, action: #selector(getPriceOrRedirect1), for: .touchUpInside)
+      
+      /* ----------------------------- RECIPE DETAILS 2 -----------------------------------------------*/
       // Configure the label
-      changeStoreLabel.translatesAutoresizingMaskIntoConstraints = false
-      changeStoreLabel.text = "Has Store"
-      changeStoreLabel.textAlignment = .center
+      recipeDetailsLabel2.translatesAutoresizingMaskIntoConstraints = false
+      recipeDetailsLabel2.text = "Sample Recipe 2"
+      recipeDetailsLabel2.textAlignment = .center
       
       // Configure the button
-      changeStoreToggle.translatesAutoresizingMaskIntoConstraints = false
-      changeStoreToggle.isOn = true
-      
-      // Add target to handle value change
-      changeStoreToggle.addTarget(self, action: #selector(changeStoreChanged(_:)), for: .valueChanged)
-      
-      /* ----------------------------- CHANGE USER -----------------------------------------------*/
-      // Configure the label
-      changeUserLabel.translatesAutoresizingMaskIntoConstraints = false
-      changeUserLabel.text = "Change User"
-      changeUserLabel.textAlignment = .center
+      recipeDetailsButton2.translatesAutoresizingMaskIntoConstraints = false
+      recipeDetailsButton2.setTitle("Launch", for: .normal)
+      recipeDetailsButton2.addTarget(self, action: #selector(launchRecipeDetails2), for: .touchUpInside)
       
       // Configure the button
-      changeUserToggle.translatesAutoresizingMaskIntoConstraints = false
-      changeUserToggle.isOn = true
-      
-      // Add target to handle value change
-      changeUserToggle.addTarget(self, action: #selector(changeUserChanged(_:)), for: .valueChanged)
-      
-      /* ----------------------------- PRICE OF RECIPE -----------------------------------------------*/
-      // Configure the button
-      priceOfRecipeButton.translatesAutoresizingMaskIntoConstraints = false
-      priceOfRecipeButton.setTitle(priceOfRecipe ?? "Get Price", for: .normal)
-      priceOfRecipeButton.addTarget(self, action: #selector(getPriceOrRedirect), for: .touchUpInside)
+      priceOfRecipeButton2.translatesAutoresizingMaskIntoConstraints = false
+      priceOfRecipeButton2.setTitle(priceOfRecipe2 ?? "Get Price", for: .normal)
+      priceOfRecipeButton2.addTarget(self, action: #selector(getPriceOrRedirect2), for: .touchUpInside)
       
       // Add the label and button to the view hierarchy
-      view.addSubview(recipeDetailsLabel)
-      view.addSubview(recipeDetailsButton)
-      view.addSubview(changeStoreLabel)
-      view.addSubview(changeStoreToggle)
-      view.addSubview(changeUserLabel)
-      view.addSubview(changeUserToggle)
-      view.addSubview(priceOfRecipeButton)
+      view.addSubview(recipeDetailsLabel1)
+      view.addSubview(recipeDetailsButton1)
+      view.addSubview(priceOfRecipeButton1)
+      view.addSubview(recipeDetailsLabel2)
+      view.addSubview(recipeDetailsButton2)
+      view.addSubview(priceOfRecipeButton2)
       
       // Constraints for the label
       NSLayoutConstraint.activate([
-         recipeDetailsLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-         recipeDetailsLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20), // 20 points from the top of the safe area
-         recipeDetailsLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
-         recipeDetailsLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
+         recipeDetailsLabel1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         recipeDetailsLabel1.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20), // 20 points from the top of the safe area
+         recipeDetailsLabel1.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+         recipeDetailsLabel1.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
       ])
       
       // Constraints for the button
       NSLayoutConstraint.activate([
-         recipeDetailsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-         recipeDetailsButton.topAnchor.constraint(equalTo: recipeDetailsLabel.bottomAnchor, constant: 20), // Position button below the label
-         recipeDetailsButton.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
-         recipeDetailsButton.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
-      ])
-      NSLayoutConstraint.activate([
-         changeStoreLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-         changeStoreLabel.topAnchor.constraint(equalTo: recipeDetailsButton.bottomAnchor, constant: 20), // Position button below the label
-         changeStoreLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
-         changeStoreLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
-      ])
-      NSLayoutConstraint.activate([
-         changeStoreToggle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-         changeStoreToggle.topAnchor.constraint(equalTo: changeStoreLabel.bottomAnchor, constant: 20), // Position button below the label
-         changeStoreToggle.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
-         changeStoreToggle.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
+         recipeDetailsButton1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         recipeDetailsButton1.topAnchor.constraint(equalTo: recipeDetailsLabel1.bottomAnchor, constant: 20), // Position button below the label
+         recipeDetailsButton1.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+         recipeDetailsButton1.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
       ])
       
       NSLayoutConstraint.activate([
-         changeUserLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-         changeUserLabel.topAnchor.constraint(equalTo: changeStoreToggle.bottomAnchor, constant: 20), // Position button below the label
-         changeUserLabel.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
-         changeUserLabel.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
-      ])
-      NSLayoutConstraint.activate([
-         changeUserToggle.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-         changeUserToggle.topAnchor.constraint(equalTo: changeUserLabel.bottomAnchor, constant: 20), // Position button below the label
-         changeUserToggle.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
-         changeUserToggle.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
+         priceOfRecipeButton1.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         priceOfRecipeButton1.topAnchor.constraint(equalTo: recipeDetailsButton1.bottomAnchor, constant: 20), // Position button below the label
+         priceOfRecipeButton1.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+         priceOfRecipeButton1.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
       ])
       
       NSLayoutConstraint.activate([
-         priceOfRecipeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-         priceOfRecipeButton.topAnchor.constraint(equalTo: changeUserToggle.bottomAnchor, constant: 20), // Position button below the label
-         priceOfRecipeButton.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
-         priceOfRecipeButton.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
+         recipeDetailsLabel2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         recipeDetailsLabel2.topAnchor.constraint(equalTo: priceOfRecipeButton1.bottomAnchor, constant: 20), // Position button below the label
+         recipeDetailsLabel2.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+         recipeDetailsLabel2.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
+      ])
+      
+      NSLayoutConstraint.activate([
+         recipeDetailsButton2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         recipeDetailsButton2.topAnchor.constraint(equalTo: recipeDetailsLabel2.bottomAnchor, constant: 20), // Position button below the label
+         recipeDetailsButton2.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+         recipeDetailsButton2.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
+      ])
+      
+      NSLayoutConstraint.activate([
+         priceOfRecipeButton2.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         priceOfRecipeButton2.topAnchor.constraint(equalTo: recipeDetailsButton2.bottomAnchor, constant: 20), // Position button below the label
+         priceOfRecipeButton2.leadingAnchor.constraint(greaterThanOrEqualTo: view.leadingAnchor, constant: 20),
+         priceOfRecipeButton2.trailingAnchor.constraint(lessThanOrEqualTo: view.trailingAnchor, constant: -20)
       ])
       
       // Additional UI setup
       view.backgroundColor = .white
    }
    
-   @objc func launchRecipeDetails() {
-      // if they don't have a store:
-      // launch store locator
-      // else
-      // launch Recipe Details
+   @objc func launchRecipeDetails1() {
+      let recipeDetails = StandaloneRecipeDetailsViewController(recipeId: "22509")
+      if let nav = self.navigationController {
+         nav.present(recipeDetails, animated: true)
+      }
+   }
+   
+   @objc func launchRecipeDetails2() {
       let recipeDetails = StandaloneRecipeDetailsViewController(recipeId: "14472")
       if let nav = self.navigationController {
          nav.present(recipeDetails, animated: true)
       }
    }
    
-   @objc func getPriceOrRedirect() {
-      if priceOfRecipe == nil {
+   @objc func getPriceOrRedirect1() {
+      if priceOfRecipe1 == nil {
          Task {
             do {
-               let price = try await Mealz.shared.recipe.getPriceOrRedirect(recipeId: "14472", numberOfGuest: 4).await()
-               self.priceOfRecipe = String(price as? Double ?? 0)
+               let price = try await Mealz.shared.recipe.getPriceOrRedirect(recipeId: "22509", numberOfGuest: 4).await()
+               let priceOfRecipe = price as? Double ?? 0
+               if priceOfRecipe == 0.0 { return }
+               self.priceOfRecipe1 = String(priceOfRecipe)
                DispatchQueue.main.async { [weak self] in
-                  self?.priceOfRecipeButton.setTitle(self?.priceOfRecipe ?? "Get Price", for: .normal)
+                  self?.priceOfRecipeButton1.setTitle(self?.priceOfRecipe1 ?? "Get Price", for: .normal)
                }
             } catch {
                DispatchQueue.main.async { [weak self] in
-                  self?.priceOfRecipeButton.setTitle("Failed to get price", for: .normal)
+                  self?.priceOfRecipeButton1.setTitle("Failed to get price", for: .normal)
                }
             }
          }
       }
    }
    
-   @objc func changeStoreChanged(_ sender: UISwitch) {
-      if sender.isOn {
-         Mealz.shared.user.setStoreWithMealzId(storeId: "25910")
-      } else { Mealz.shared.user.setStoreWithMealzId(storeId: "") }
-   }
-   
-   @objc func changeUserChanged(_ sender: UISwitch) {
-      if sender.isOn {
-         Mealz.shared.user.updateUserId(userId: "test_\(UUID())", authorization: Authorization.userId)
-      } else {
-         Mealz.shared.user.updateUserId(userId: nil, authorization: Authorization.userId)
+   @objc func getPriceOrRedirect2() {
+      if priceOfRecipe2 == nil {
+         Task {
+            do {
+               let price = try await Mealz.shared.recipe.getPriceOrRedirect(recipeId: "14472", numberOfGuest: 4).await()
+               let priceOfRecipe = price as? Double ?? 0
+               if priceOfRecipe == 0.0 { return }
+               self.priceOfRecipe1 = String(priceOfRecipe)
+               DispatchQueue.main.async { [weak self] in
+                  self?.priceOfRecipeButton2.setTitle(self?.priceOfRecipe2 ?? "Get Price", for: .normal)
+               }
+            } catch {
+               DispatchQueue.main.async { [weak self] in
+                  self?.priceOfRecipeButton2.setTitle("Failed to get price", for: .normal)
+               }
+            }
+         }
       }
    }
 }
